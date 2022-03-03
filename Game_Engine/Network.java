4adae3f1165a04373b490f3e85307225fc968932
EduSScs <eduardosstorreon@gmail.com>
2022-03-03 18:53:33 -0500

    creted objecct

--- Game_Engine/Network.java
+++ Game_Engine/Network.java
@@ -4,6 +4,13 @@ import UI.*; //to be used later.
 
 public class Network {
 
+
+    int x, y;
+    public Network(){
+        x = 20;
+        y = 20;
+    }
+
     public void display() {
         // For each interesection and road, draw a connection.
         // For each road, change color depending on capacity.
@@ -11,6 +18,7 @@ public class Network {
 
     private void Read_Graph() {
         // Take user input to generate a road map.
+        int[][] graph = new int[x][y];
     }
 
 
--- Traffic_Game/game.java
+++ Traffic_Game/game.java
@@ -6,7 +6,6 @@ public class game {
 
     public static void main(String[] args) {
         Game_Engine.Network n = new Game_Engine.Network();
-
     }
 
 }
