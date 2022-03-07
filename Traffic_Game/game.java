de60f61edb02d0b54516c86fec9ca70502470d9f
Alex <32850744+RABlue27@users.noreply.github.com>
2022-03-06 20:27:09 -0500

    Update game.java

--- Traffic_Game/game.java
+++ Traffic_Game/game.java
@@ -6,6 +6,11 @@ public class game {
 
     public static void main(String[] args) {
         Game_Engine.Network n = new Game_Engine.Network();
+        // create a loop that runs until the games over (because the player crashed)
+        while (n.isRunning) {
+            // call the step function
+            n.step();
+        }
     }
 
 }
